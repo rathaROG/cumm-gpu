656bf6520e622dadaefc135755065a131bb2ae34
rathaROG <10178493+rathaROG@users.noreply.github.com>
2026-04-02 03:46:12 +0200

    Enhance handling of CUDA DLL discovery on Windows

--- cumm/__init__.py
+++ cumm/__init__.py
@@ -1,3 +1,5 @@
+# Modified by rathaROG in 2026.
+
 # Copyright 2024 Yan Yan
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,20 +28,24 @@ from pathlib import Path
 import importlib.util 
 import subprocess
 
-if project_is_installed(PACKAGE_NAME) and project_is_editable(
-        PACKAGE_NAME) and not CUMM_DISABLE_JIT:
+
+if project_is_installed(PACKAGE_NAME) and project_is_editable(PACKAGE_NAME) and not CUMM_DISABLE_JIT:
     from cumm.csrc.arrayref import ArrayPtr
     from cumm.tensorview_bind import TensorViewBind, AppleMetalImpl
-    pccm.builder.build_pybind([ArrayPtr(), TensorViewBind(), AppleMetalImpl()],
-                              PACKAGE_ROOT / "core_cc",
-                              namespace_root=PACKAGE_ROOT,
-                              load_library=False,
-                              std="c++17" if compat.InMacOS else "c++14",
-                              verbose=False)
+    pccm.builder.build_pybind(
+        [ArrayPtr(), TensorViewBind(), AppleMetalImpl()],
+        PACKAGE_ROOT / "core_cc",
+        namespace_root=PACKAGE_ROOT,
+        load_library=False,
+        std="c++17" if compat.InMacOS else "c++14",
+        verbose=False
+    )
+
 
 def _determine_windows_cuda_dll_dir():
     cuda_dll_paths = []
     res_msgs = []
+
     # step 1: check pip installed nvidia package (e.g. nvidia-cuda-runtime-cu12)
     try:
         import nvidia 
@@ -48,11 +54,8 @@ def _determine_windows_cuda_dll_dir():
         res = [Path(cuda_runtime.__file__).resolve().parent / "bin", Path(cuda_nvrtc.__file__).resolve().parent / "bin"]
         return res, res_msgs
     except:
-        res_msgs.append("Step 1: Can't find pip nvidia cuda package, "
-                "you may need to install nvidia-cuda-runtime-cu12 and "
-                "nvidia-cuda-nvrtc-cu12 by pip. WARNING: "
-                "use pip don't support runtime kernels because"
-                "headers are missing.")
+        res_msgs.append("\nStep 1: Couldn't find pip NVIDIA CUDA package." \
+                        "\n\tWARNING: pip NVIDIA CUDA package doesn't support runtime kernels.")
 
     # step 2: try to load cuda dll from torch path
     spec = importlib.util.find_spec("torch")
@@ -64,56 +67,63 @@ def _determine_windows_cuda_dll_dir():
         if cudart_paths and nvrtc_paths:
             cuda_dll_paths = [str(torch_path / "lib")]
             return cuda_dll_paths, res_msgs
-
-    res_msgs.append("Step 2: Can't find pytorch cuda libraries "
-            "you may need to install cuda pytorch first.")
+    res_msgs.append("\nStep 2: Couldn't find PyTorch CUDA libraries.")
 
     # step 3: check conda installed nvidia package (e.g. conda install cuda -c nvidia) 
     # use nvcc path to find cuda dll path
-    # you may need to install to base env because
-    # powershell don't know which env you are using
-    try:
-        nvcc_path = subprocess.check_output(["powershell", "-command", "(Get-Command nvcc).Source"
-                                            ]).decode("utf-8").strip()
-        nvcc_root = Path(nvcc_path).parent.parent
-        lib = nvcc_root / "lib"
-        include = nvcc_root / "include"
-        if lib.exists() and include.exists():
-            cuda_dll_paths = [nvcc_root / "bin"]
-            return cuda_dll_paths, res_msgs
-    except:
-        pass
-    res_msgs.append("Step 3: Can't find conda nvidia cuda package, "
-            "you may need to install cuda by `conda install cuda -c nvidia` to base env.")
-    # step 3: try to load cuda dll from system installed cuda path
+    conda_prefix = os.environ.get("CONDA_PREFIX")
+    if conda_prefix:
+        try:
+            nvcc_path = subprocess.check_output(["powershell", "-command", "(Get-Command nvcc).Source"]).decode("utf-8").strip()
+            conda_prefix_norm = os.path.abspath(conda_prefix).lower()
+            nvcc_path_norm = os.path.abspath(nvcc_path).lower()
+            if nvcc_path_norm.startswith(conda_prefix_norm + os.sep):
+            # if Path(conda_prefix) in Path(nvcc_path).parents:
+                nvcc_root = Path(nvcc_path).parent.parent
+                lib = nvcc_root / "lib"
+                include = nvcc_root / "include"
+                if lib.exists() and include.exists():
+                    cuda_dll_paths = [nvcc_root / "bin"]
+                    if (Path(nvcc_root) / "bin" / "x64").exists():
+                        cuda_dll_paths.append(Path(nvcc_root) / "bin" / "x64")
+                    return cuda_dll_paths, res_msgs
+        except:
+            pass
+    res_msgs.append("\nStep 3: Couldn't find conda NVIDIA CUDA package.")
+
+    # step 4: try to load cuda dll from system installed cuda path
     cuda_path = os.getenv("CUDA_PATH", None)
-    if cuda_path is not None and (Path(cuda_path) / "bin").exists():
-        return [Path(cuda_path) / "bin"], res_msgs
+    if cuda_path and (Path(cuda_path)).exists():
+        cuda_dll_paths = [Path(cuda_path) / "bin"]
+        if (Path(cuda_path) / "bin" / "x64").exists():
+            cuda_dll_paths.append(Path(cuda_path) / "bin" / "x64")
+        res_msgs.append(f"\nStep 4: Found CUDA Toolkit in system env CUDA_PATH.")
+        return cuda_dll_paths, res_msgs
 
-    res_msgs.append("Step 4: Can't find system cuda or system cuda version mismatch, "
-            "you may need to download and install cuda")
+    res_msgs.append("\nStep 4: Couldn't find system installed CUDA Toolkit.")
 
-
-    res_msgs.append("can't find cuda dll path, try method above.")
     return cuda_dll_paths, res_msgs
+
 
 if compat.InWindows:
     try:
         from cumm import core_cc
     except:
         cuda_dll_paths, msgs = _determine_windows_cuda_dll_dir()
+        
         if not cuda_dll_paths:
             print("\n".join(msgs), file=sys.stderr)
-            raise RuntimeError("Can't find cuda libraries, install cuda first.")
+            raise RuntimeError("Couldn't find cuda libraries, install cuda first.")
         for p in cuda_dll_paths:
             os.add_dll_directory(str(p))
         try:
             from cumm import core_cc
         except:
+            if cuda_dll_paths:
+                print("\nCUMM found CUDA *.dll here:", cuda_dll_paths, file=sys.stderr)
+                print("\n*** BUT couldn't load due to version mismatched? ***", file=sys.stderr)
+            else:
+                print("\nCUMM couldn't find any CUDA *.dll in the system.", file=sys.stderr)
             print("\n".join(msgs), file=sys.stderr)
-            print(f"We still can't load correct cuda libs from {cuda_dll_paths}, "
-                  "you need to check your installed cuda version is compatible with "
-                  "installed cumm version. e.g. you must install cuda 12 "
-                  "(via pip, conda or pytorch with cuda), not 11, if use"
-                  "cumm-cu124, cumm-cu121 or cumm-cu126.", file=sys.stderr)
+            print("\n*** Please install CUMM/CUDA with correct version. ***\n", file=sys.stderr)
             raise 
